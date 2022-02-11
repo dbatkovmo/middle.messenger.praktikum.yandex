8a4555bb6221b3419950f742d88086b13e4d8eba
D.Batkov <P@SSword>
2022-02-11 13:17:24 +0300

    Routes

--- server.js
+++ server.js
@@ -1,10 +1,43 @@
 const express = require('express');
-
 const app = express();
 const PORT = 3000;
 const port = process.env.PORT || PORT;
+const filesDirectory = `${__dirname}/dist`;
+const routes = [
+    {
+        path: '/',
+        file: '/login.html'
+    },
+    {
+        path: '/login',
+        file: '/login.html'
+    },
+    {
+        path: '/registration',
+        file: '/registration.html'
+    },
+    {
+        path: '/settings-user',
+        file: '/settings-user.html'
+    },
+    {
+        path: '/chat',
+        file: '/chat.html'
+    }
+]
+
 
 app.use(express.static(`${__dirname}/dist`));
+
+routes.forEach(({ path, file }) => {
+    app.get(path, function(req, res) {
+        res.sendFile(`${filesDirectory}${file}`);
+    });
+})
+
+app.use(function (req, res, next) {
+    res.status(404).sendFile(`${filesDirectory}/404.html`);
+});
 
 app.listen(port, () => {
     console.log(`Server http://localhost:3000/`);
