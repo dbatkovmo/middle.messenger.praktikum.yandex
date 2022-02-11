ed895613798b6389755d528690b4505feb68139e
D.Batkov <P@SSword>
2022-02-11 14:44:15 +0300

    Доработка

--- server.js
+++ server.js
@@ -34,12 +34,12 @@ routes.forEach(({ path, file }) => {
     });
 })
 
-// app.use('*/*', (req, res) => {
-//     res.sendFile(`${filesDirectory}/login.html`);
-// })
+app.get('/*', (req, res) => {
+    res.sendFile(`${filesDirectory}/login.html`);
+});
 
 app.use(function (req, res, next) {
-    res.status(404).sendFile(`${filesDirectory}/login.html`);
+    res.status(404).sendFile(`${filesDirectory}/404.html`);
 });
 
 app.listen(port, () => {
